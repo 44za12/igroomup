fa74d5941d10231d4ae50a265e949289b0d2132f
Aazar <44za12@gmail.com>
2020-05-18 02:09:21 +0530

    Deploying on heroku

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def downloadFile ():
     path = "brochure.pdf"
     return send_file(path, as_attachment=True)
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 4444))
+    port = int(os.environ.get('PORT', 4444))
     app.run(debug=True, host='0.0.0.0', port=port)
